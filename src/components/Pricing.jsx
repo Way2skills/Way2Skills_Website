c553548c92e0250a8ce373adfeaed43437676778
Sachinn-p <sachinn2413@gmail.com>
2025-01-21 16:54:00 +0530

    bg changed

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -10,7 +10,7 @@ const Pricing = () => {
       <div className="flex flex-wrap">
         {pricingOptions.map((option, index) => (
           <div key={index} className="w-full sm:w-1/2 lg:w-1/3 p-2">
-            <div className="p-10 border border-neutral-700 rounded-xl">
+            <div className="p-10 border border-neutral-900 rounded-xl">
               <p className="text-4xl mb-8">
                 {option.title}
                 {option.mode === "online" && (
