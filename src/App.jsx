434178fabc3a2965bfe022234926175e4d7fa74b
Akash2king <logeshwaran.v@kongunadu.ac.in>
2025-02-05 07:24:54 +0530

    redirect bloger to /

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import AboutUs from "./components/AboutUs";
 import Home from "./components/Home";
 import Courses from "./components/Courses";
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import CourseRegistrationForm from "./components/CourseRegistrationForm";
 const App = () => {
   return (
@@ -12,6 +12,8 @@ const App = () => {
     <Route path="/" element={<Home/>}/>
     <Route path="/about-us" element={<AboutUs/>}/>
     <Route path="/Courses" element={<Courses/>}/>
+    <Route path="/blogger" element={<Navigate to="/" />}/>
+    <Route path="*" element={<Navigate to="/" />} />
     {/* <Route path="/Register" element={<CourseRegistrationForm/>}/> */}
    </Routes>
    </BrowserRouter>
